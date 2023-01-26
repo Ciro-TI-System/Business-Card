b1c5cd27474335a9db1023df355e7236b324c436
Ciro Batista da Silva <78507494+Ciro-TI-System@users.noreply.github.com>
2023-01-26 20:57:59 -0300

    Updating 'alt' attributes per mode.

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ function toggleMode() {
   const img = document.querySelector("#profile img");
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light-mode.png");
+    img.setAttribute("alt", "Avatar 'light-mode' do Discord");
   } else {
     img.setAttribute("src", "./assets/avatar-dark-mode.png");
+    img.setAttribute("alt", "Avatar 'dark-mode' do Discord");
   }
 }
